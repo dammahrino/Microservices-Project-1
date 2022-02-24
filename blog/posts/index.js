5a9d1b4dedb154cb44fbfd5641daa395e006a54b
Daniel Cadena <dacadadenadi@gmail.com>
2022-02-23 22:42:07 -0600

    Base configuration for the project, including the react app, and the setup for the posts and comments microservice

---
+++ blog/posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+
+// Object for storing locally the posts
+const posts = {};
+
+// Route for retrieving the posts
+app.get('/posts', (request, response) => {
+    response.send(posts);
+});
+
+// Route for posting 'posts'
+app.post('/posts', (request, response) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = request.body;
+
+    posts[id] = { id, title };
+
+    response.status(201).send(posts[id]);
+});
+
+
+// Set the port in which the application will be listening.
+app.listen(4000, () => {
+    console.log('Listening on 4000.')
+})
